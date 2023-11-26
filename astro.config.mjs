247f0142dd444c26708975f7f7eac5c2d7ad1668
Hrihorii Ilin <devopsick@pm.me>
2023-11-26 00:55:49 +0000

    Make changes with translate and make base.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import yaml from '@rollup/plugin-yaml';
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()]
+  },
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })],
